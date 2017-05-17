b295a365f32c4fc5b67688413f9bab2373e17ba7
MiloFerguson19 <miloferguson19@students.oakwoodschool.org>
2017-05-17 11:56:45 -0700

    Add files via upload

---
+++ process.php
@@ -0,0 +1,18 @@
+<?php
+
+    $to = "MiloFerguson19@students.oakwoodschool.org", "st2038@students.oakwoodschool.org";
+    $from = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"subject"} = "subject";
+    $fields{"message"} = "message";
+
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $send = mail($to, $subject, $body, $headers);
+?>
